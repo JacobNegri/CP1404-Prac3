91e37f66a9cc9b3a5eab13e815b1cbdae53456b3
jc319816 <jacob.negri@my.jcu.edu.au>
2016-03-30 12:47:19 +1000

    updated

--- str_ASCII_table shown.py
+++ str_ASCII_table shown.py
@@ -1,8 +1,19 @@
 
-lower = 10
-upper = 100
+def main():
 
-print("Enter a number {} , {}".format(lower,upper))
+    lower = 10
+    upper = 50
 
-for i in range(10, 120, 11): # make sure we have integers of different 'length'
- print("{} {}".format(i, chr(i)))
+    numbers = get_number(lower,upper)
+    print(numbers)
+
+    ##for i in range(10, 120, 11): # make sure we have integers of different 'length'
+    ##print("{} {}".format(i, chr(i)))
+
+def get_number(lower,upper):
+
+    values = input("Enter a number {} , {}".format(lower,upper))
+    while not values.isnumeric() or int(values) < int(lower) or int(values) > int(upper):
+        values = input("Please Enter a correct set of numbers {} , {}".format(lower,upper))
+    return values
+main()
